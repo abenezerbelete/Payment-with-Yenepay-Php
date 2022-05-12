0c2dad1174d1968df8ec1ba0d6d1b07362ab5c94
Abenezer <abenezerbelete458@gmail.com>
2022-05-12 15:17:23 +0300

    Initializations

---
+++ payment-with-express.php
@@ -0,0 +1,11 @@
+<!Doctype html>
+<html>
+    <title>Payment</title>
+    <body>
+        <h2>Pay with Yenepay</h2>
+        <form method="post" action="https://test.yenepay.com/">
+            <input type="hidden" name="x" value="y">
+            <button type="submit">pay</button>
+        </form>
+    </body>
+</html>
