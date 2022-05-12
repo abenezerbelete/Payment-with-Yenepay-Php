8611c2a15f6d2a25996a55373a8e61eb5f70ef03
Abenezer <abenezerbelete458@gmail.com>
2022-05-12 16:55:10 +0300

    expanding express method

--- payment-with-cart.php
+++ payment-with-cart.php
@@ -2,7 +2,7 @@
 <html>
     <title>Payment</title>
     <body>
-        <h2>Pay with Yenepay </h2>
+        <h2>Pay with Yenepay</h2>
         <form method="post" action="https://test.yenepay.com/">
             <input type="hidden" name="process" value="Express">
             <input type="hidden" name="successUrl" value="http://localhost/Payment-with-Yenepay-Php/success.php">
@@ -11,16 +11,12 @@
             <input type="hidden" name="merchantId" value="SB1151">
             <input type="hidden" name="merchantOrderId" value="xy2">
             <input type="hidden" name="expiresAfter" value="24">
-            <input type="hidden" name="itemId" value="50">
-            <input type="hidden" name="itemName" value="Billing">
-            <input type="hidden" name="unitPrice" value="105.75">
-            <input type="hidden" name="quantity" value="5">
-            <input type="hidden" name="discount" value="0.5">
-            <input type="hidden" name="handlingFee" value="0">
-            <input type="hidden" name="deliveryFee" value="0">
-            <input type="hidden" name="tax1" value="0">
-            <input type="hidden" name="tax2" value="0">
-            <button type="submit">pay with express</button>
+            <input type="hidden" name="totalItemsDeliveryFee" value="10">
+            <input type="hidden" name="totalItemsDiscount" value="5">
+            <input type="hidden" name="totalItemsHandlingFee" value="2.5">
+            <input type="hidden" name="totalItemsTax1" value="300">
+            <input type="hidden" name="totalItemsTax2" value="24">
+            <button type="submit">pay with cart</button>
         </form>
     </body>
 </html>
---
+++ success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paid</title>
+</head>
+<body>
+    <h3>You've successfully paid <?php echo $_GET["TotalAmount"] ?>! Thank you!</h3>
+    <a href="payment-with-express.php">to pay again with express</a>
+</body>
+</html>
